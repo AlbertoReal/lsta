2b3d8da7a9c501596f0993f35351661f931442fa
AlbertoReal <32370393+AlbertoReal@users.noreply.github.com>
2019-03-05 18:07:10 +0100

    Add files via upload

---
+++ musica.php
@@ -0,0 +1,81 @@
+
+<?php
+header("access-control-allow-origin: *");
+?>
+
+{
+	"canciones":[
+		{
+			"id": "1",
+			"titulo": "karma",
+			"autor": "kamelot",
+			"genero": "rock",
+			"duracion": "307"
+		},
+		{
+			"id": "2",
+			"titulo":"acros the highlands",
+			"autor": "kamelot",
+			"genero" : "rock",
+			"duracion": "207"
+
+		},
+		{
+			"id":"3",
+			"titulo": "mournig star",
+			"autor": "kamelot",
+			"genero": "rock",
+			"duracion": "259" 
+		},
+		{
+			"id": "4",
+			"titulo": "the kinslayer",
+			"autor": "nightwish",
+			"genero": "opera-rock",
+			"duracion": "240" 
+		},
+		{
+			"id": "5",
+			"titulo": "black diamnond",
+			"autor": "stratovarius",
+			"genero": "rock",
+			"duracion": "322"
+		},
+		{
+			"id":"6",
+			"titulo": "soul society",
+			"autor": "kamelot",
+			"genero": "rock",
+			"duracion": "250"
+		},
+		{
+			"id": "7",
+			"titulo": "uta",
+			"autor": "imperial circus dead decadence",
+			"genero": "metal",
+			"duracion": "542"
+		},
+		{
+			"id": "8",
+			"titulo": "in the midle of the night",
+			"autor": "within temptation",
+			"genero": "opera-rock",
+			"duracion": "314"
+		},
+		{
+			"id": "9",
+			"titulo": "ways to epica",
+			"autor": "kamelot",
+			"genero": "rock",
+			"duracion": "370"
+		},
+		{
+			"id": "10",
+			"titulo": "center of the universe",
+			"autor": "kamelot",
+			"genero": "rock",
+			"duracion": "316"
+		}
+
+	]
+}
